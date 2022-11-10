eb4e45a90fe8cde483deb4edb155a84190702f4e
MR436 <mandeepr78@gmail.com>
2022-11-10 15:57:59 +0000

    Task 4 completed

--- milestone_2.py
+++ milestone_2.py
@@ -7,12 +7,14 @@ print(word)
 print("The random word is:", (word))
 guess = input("Please enter a single letter:")
 print(guess)
-#while True:
-    #guess = input("Please enter a single letter:")
-    #if len(guess)>1:
-        #print("Good guess!")
-    #else: 
-        #print("Oops! That is not a valid input.")
+while True:
+    guess = input("Please enter a single letter:")
+    if len(guess)>1 and (guess).isalpha:
+        print("Good guess!")
+    else: 
+        print("Oops! That is not a valid input.")
+        break
+    
         
 
 
