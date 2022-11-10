c905b49be2016b69e8b618901270015a444b2bed
MR436 <mandeepr78@gmail.com>
2022-11-10 14:50:08 +0000

    task 1 completed

---
+++ milestone_2.py
@@ -0,0 +1,21 @@
+#import random
+#from random import choice
+word_list = ["Guava", "Banana","Apple", "Blueberry", "Mango"]
+print(word_list)
+#word = random.choice(word_list)
+#print(word)
+#print("The random word is:", (word))
+#guess = input("Please enter a single letter:")
+#print(guess)
+#while True:
+    #guess = input("Please enter a single letter:")
+    #if len(guess)>1:
+        #print("Good guess!")
+    #else: 
+        #print("Oops! That is not a valid input.")
+        
+
+
+
+
+
