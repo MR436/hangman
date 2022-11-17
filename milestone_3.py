5ea68366a933d318662b6cbdd5a7e51169aa2a2c
MR436 <mandeepr78@gmail.com>
2022-11-17 21:15:02 +0000

    Task2 completed

--- milestone_3.py
+++ milestone_3.py
@@ -1,9 +1,8 @@
-while True:
-    guess = input("Please enter a single letter:")
-    if len(guess)>1 and (guess).isalpha:
-        break
-    else:
-        print("Invalid letter. Please, enter a single alphabetical character.") 
-
+word = "apple"
+guess = input("Please enter a single letter:")
+if guess in word:
+    print(f"Good guess! {guess} is in the word.") 
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
 
  
