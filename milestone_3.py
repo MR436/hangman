54621ed9a5fe861cd371f25bbd12799bad494f98
MR436 <mandeepr78@gmail.com>
2022-11-17 21:44:52 +0000

    Task2 completed

--- milestone_3.py
+++ milestone_3.py
@@ -5,11 +5,11 @@ while True:
         break
     else:
         print("Oops! That is not a valid input.")
-#word = "apple"
-#guess = input("Please enter a single letter:")
-#if guess in word:
-    #print(f"Good guess! {guess} is in the word.") 
-#else:
-    #print(f"Sorry, {guess} is not in the word. Try again.")
+word = "apple"
+guess = input("Please enter a single letter:")
+if guess in word:
+    print(f"Good guess! {guess} is in the word.") 
+else:
+    print(f"Sorry, {guess} is not in the word. Try again.")
 
  
