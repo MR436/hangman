4ceda5d0aa42b36c3c39d341fb427cbb9bb960eb
MR436 <mandeepr78@gmail.com>
2022-11-24 10:12:47 +0000

    Task 3 completed

--- milestone_3.py
+++ milestone_3.py
@@ -1,15 +1,24 @@
-while True:
-    guess = input("Please enter a single letter:")
-    if len(guess)>1 and (guess).isalpha:
-        print("Good guess!")
-        break
-    else:
-        print("Oops! That is not a valid input.")
 word = "apple"
-guess = input("Please enter a single letter:")
-if guess in word:
-    print(f"Good guess! {guess} is in the word.") 
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
+def check_guess(guess):
+    guess = guess.lower()
+    if guess in word:
+        print(f"Good guess! {guess} is in the word.") 
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again.")
+
+def ask_for_input():
+    while True:
+        guess = input("Please enter a single letter:")
+        if guess.isalpha() and len(guess) == 1:
+            print("Good guess!")
+            break
+        else:
+            print("Oops! That is not a valid input.")
+    check_guess(guess)
+
+ask_for_input()
+
+
+
 
  
