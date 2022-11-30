d97009f8d4358d36d457bd677e78d1257b3a5a1b
MR436 <mandeepr78@gmail.com>
2022-11-30 15:28:01 +0000

    Task 2 completed

--- milestone_4.py
+++ milestone_4.py
@@ -1,49 +1,51 @@
 import random
 from random import choice
+word_list = ["Guava", "Banana","Apple", "Blueberry", "Mango"]
+
 class Hangman():
-    def __init__(self, word, word_guessed, num_letters, word_list, list_of_guesses, num_lives = 5, ):
-        self.word = word
-        self.word_guessed = word_guessed
-        self.num_letters = num_letters
+    def __init__(self, word_list, num_lives = 5):
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_'] * len(self.word)
+        self.num_letters = len(set(self.word))
         self.num_lives = num_lives
         self.word_list = word_list
-        self.list_of_guesses = list_of_guesses
+        self.list_of_guesses = []
+    
+    def check_guess (self, guess):
+        guess = guess.lower()
+        if  guess in self.word:
+            print(f"Good guess! {guess} is in the word.") 
+            #self.list_of_guesses.append(guess) 
+        else: 
+            print(f"Sorry, {guess} is not in the word. Try again.")
+        self.list_of_guesses.append(guess)
 
-        #word_list = ["Guava", "Banana","Apple", "Blueberry", "Mango"]
-        #num_lives = 5
-        
+    def ask_for_input(self):
+        while True:
+            guess = input("Please enter a single letter:")
+            if guess.isalpha() and len(guess) == 1:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in [self.list_of_guesses]:
+                print("You already tried that letter!")
+            else:
+                guess = self.check_guess
+                
+player1 = Hangman(word_list)
+player1.ask_for_input()
 
-        word_list =  ['_', '_', '_', '_', '_']
-        word = random.choice(word_list)
+
+
+
+
+
+
+
 
        
 
 
 
-
-        pass
     
-
-
-def check_guess(guess):
-    guess = guess.lower()
-    if guess in word:
-        print(f"Good guess! {guess} is in the word.") 
-    else:
-        print(f"Sorry, {guess} is not in the word. Try again.")
-
-def ask_for_input():
-    while True:
-        guess = input("Please enter a single letter:")
-        if guess.isalpha() and len(guess) == 1:
-            print("Good guess!")
-            break
-        else:
-            print("Oops! That is not a valid input.")
-    check_guess(guess)
-
-ask_for_input()
-
 
 
 
