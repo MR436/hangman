cac19fc5a29b2cbe44f3646753be4fe3d5d911f4
MR436 <mandeepr78@gmail.com>
2022-12-07 12:01:32 +0000

    Task 4 completed

--- milestone_4.py
+++ milestone_4.py
@@ -17,14 +17,16 @@ class Hangman():
             print(f"Good guess! {guess} is in the word.") 
             for letter in range(len(self.word)):
                 if guess == self.word[letter]:
-                    self.word_guessed += guess
+                    self.word_guessed = guess
                     #self.word_guessed = guess * len(self.word)
                 else:
-                    letter += self.word_guessed[letter]
+                    letter = self.word_guessed[letter]
         else:
-            print(f"Sorry, {guess} is not in the word. Try again.")
+            self.num_lives = self.num_lives - 1
+            print(f"Sorry, {guess} is not in the word.")
+            print(f"You have {self.num_lives} lives left.")
         self.list_of_guesses.append(guess)
-        self.num_letters - 1
+        #self.num_letters - 1
 
     def ask_for_input(self):
         while True:
